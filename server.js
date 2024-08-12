5b5b8e28bf9cf4bbe6874e4fa502170e7d19ce85
zaz7 <abdullah75farid@gmail.com>
2024-08-12 15:38:00 +0300

    final commit

--- server.js
+++ server.js
@@ -1,19 +1,23 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
 
-  const posts = [
-    {
-      username: "kyle",
-      title: "Post 1",
-    },
-    {
-      username: "Jim",
-      title: "Post 2",
-    }
-  ];
-  app.get("/posts", (req, res) => {
-    console.log(json(posts));
-    
+const posts = [
+  {
+    username: "kyle",
+    title: "Post 1",
+  },
+  {
+    username: "Jim",
+    title: "Post 2",
+  },
+];
+app.get('/posts', (req, res) => {
+  try {
+    console.log(res.status(200).json(posts));
+  } catch (error) {
+    res.status(400).json({message: error.message})
+  }
 });
 app.listen(3000);
