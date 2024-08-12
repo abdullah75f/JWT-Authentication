00193e278e924b77ed7da6e9934240a2356bfdec
zaz7 <abdullah75farid@gmail.com>
2024-08-12 21:35:00 +0300

    final commit

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
 app.use(express.json());
+
+const users = [];
 
 const posts = [
   {
@@ -18,11 +21,27 @@ app.get("/posts", (req, res) => {
   try {
     res.status(200).json(posts);
   } catch (error) {
-    res.status(400).json({ message: error.message });
+    res.status(400).send();
   }
 });
 
-app.get("login", (req, res) => {
+// registration or sign up
+app.post("/users", async (req, res) => {
+  try {
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+    console.log(salt);
+    console.log(hashedPassword);
+    const user = { name: req.body.name, password: hashedPassword };
+    users.push(user);
+    res.status(200).send();
+  } catch (error) {
+    res.status(500).send();
+  }
+});
+
+app.post("login", (req, res) => {
   //Authenticate User
+  const username = req.body.name;
 });
 app.listen(3000);
